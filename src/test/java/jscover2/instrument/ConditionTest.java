91a18d91c446bdd4283dfcbbf2e1dabe20dc913b
tntim96 <tntim96@gmail.com>
2014-08-24 00:44:42 +1000

    Improve condition test

--- src/test/java/jscover2/instrument/ConditionTest.java
+++ src/test/java/jscover2/instrument/ConditionTest.java
@@ -3,44 +3,49 @@ package jscover2.instrument;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class ConditionTest {
     private ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    private Invocable invocable = (Invocable) engine;
     private Configuration config = new Configuration();
     private Instrumenter instrumenter;
 
     @Before
-    public void before() {
+    public void before() throws ScriptException {
         config.setCoverVariableName("jscover");
         instrumenter = new Instrumenter(config);
+        String code = "function condition(a, b, c) {\n" +
+                "    return ((a || b) && c);\n" +
+                "}\n";
+        String instrumented = instrumenter.instrument("test.js", code);
+        engine.eval(instrumented);
     }
 
     @Test
-    public void shouldCoverCondition() throws ScriptException {
-        String instrumented = instrumenter.instrument("test.js", "function condition(a, b, c) {\n" +
-                "    return ((a || b) && c);\n" +
-                "}\n" +
-                "condition(true, false, true);");
-        assertThat(engine.eval(instrumented), equalTo(true));
-        assertThat(engine.eval("jscover['test.js'].s['1']"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].s['2']"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].s['3']"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].s['4']"), nullValue());
-        assertThat(engine.eval("jscover['test.js'].f['1']"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].b['3'][0]"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].b['3'][1]"), equalTo(0));
-        assertThat(engine.eval("jscover['test.js'].b['4'][0]"), equalTo(0));
-        assertThat(engine.eval("jscover['test.js'].b['4'][1]"), equalTo(0));//Short-circuit
-        assertThat(engine.eval("jscover['test.js'].b['2'][0]"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].b['2'][1]"), equalTo(0));
-        assertThat(engine.eval("jscover['test.js'].b['1'][0]"), equalTo(1));
-        assertThat(engine.eval("jscover['test.js'].b['1'][1]"), equalTo(0));
+    public void shouldCoverCondition() throws ScriptException, NoSuchMethodException {
+        assertThat(invocable.invokeFunction("condition", true, false, true), is(true));
+        verify(new int[]{1, 0}, new int[]{1, 0}, new int[]{1, 0}, new int[]{0, 0}, 1);
+    }
+
+    private void verify(int[] br1, int[] br2, int[] br3, int[] br4, int calls) throws ScriptException {
+        assertThat(engine.eval("jscover['test.js'].s['1']"), equalTo(calls));
+        assertThat(engine.eval("jscover['test.js'].s['2']"), equalTo(calls));
+        assertThat(engine.eval("jscover['test.js'].s['3']"), nullValue());
+        assertThat(engine.eval("jscover['test.js'].f['1']"), equalTo(calls));
+        assertThat(engine.eval("jscover['test.js'].b['1'][0]"), equalTo(br1[0]));
+        assertThat(engine.eval("jscover['test.js'].b['1'][1]"), equalTo(br1[1]));
+        assertThat(engine.eval("jscover['test.js'].b['2'][0]"), equalTo(br2[0]));
+        assertThat(engine.eval("jscover['test.js'].b['2'][1]"), equalTo(br2[1]));
+        assertThat(engine.eval("jscover['test.js'].b['3'][0]"), equalTo(br3[0]));
+        assertThat(engine.eval("jscover['test.js'].b['3'][1]"), equalTo(br3[1]));
+        assertThat(engine.eval("jscover['test.js'].b['4'][0]"), equalTo(br4[0]));
+        assertThat(engine.eval("jscover['test.js'].b['4'][1]"), equalTo(br4[1]));
     }
 }

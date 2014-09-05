c863f91267ca882caed43fb747cd98d1eaaccf1f
tntim96 <tntim96@gmail.com>
2014-09-05 19:23:15 +1000

    Add branch path map test

--- src/test/java/jscover2/instrument/SwitchTest.java
+++ src/test/java/jscover2/instrument/SwitchTest.java
@@ -108,6 +108,9 @@ public class SwitchTest {
 
         assertThat(engine.eval("jscover['test.js'].bM['1'][0].pos.line"), equalTo(3));
         assertThat(engine.eval("jscover['test.js'].bM['2'][0].pos.line"), equalTo(11));
+        assertThat(engine.eval("jscover['test.js'].bM['1'][4]"), nullValue());
+        assertThat(engine.eval("jscover['test.js'].bM['2'][1]"), nullValue());
+        assertThat(engine.eval("jscover['test.js'].bM['3']"), nullValue());
 
         assertThat(invocable.invokeFunction("sw2", "anything"), equalTo(10));
         assertThat(engine.eval("jscover['test.js'].b['2'][0]"), equalTo(1));

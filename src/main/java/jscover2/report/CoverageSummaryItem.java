2810b5feabb4e4f2ee302d10972daac68bca9ab3
tntim96 <tntim96@gmail.com>
2014-09-07 10:11:30 +1000

    Calculate ratio once on construction

--- src/main/java/jscover2/report/CoverageSummaryItem.java
+++ src/main/java/jscover2/report/CoverageSummaryItem.java
@@ -3,13 +3,15 @@ package jscover2.report;
 public class CoverageSummaryItem {
     private int total;
     private int covered;
+    private float ratio;
 
     public CoverageSummaryItem(int total, int covered) {
         this.total = total;
         this.covered = covered;
+        this.ratio = (float) covered / total;
     }
 
     public float getRatio() {
-        return (float) covered / total;
+        return ratio;
     }
 }

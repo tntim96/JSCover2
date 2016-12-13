3891e9645938c8460f07d6e6c9d2e3fa8c971105
tntim96 <tntim96@gmail.com>
2016-12-13 13:53:41 +1100

    Replaced mockito deprecated code

--- src/test/java/jscover2/report/CoverageSummaryDataTest.java
+++ src/test/java/jscover2/report/CoverageSummaryDataTest.java
@@ -3,7 +3,7 @@ package jscover2.report;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
